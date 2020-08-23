2f8cc23b39b2615a582efd16956cd288cd09889c
kgonzalez1219 <kat@MSI.localdomain>
2020-08-22 17:56:18 -0700

    add constructor

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ commit 1 - initial files
 commit 2 - images and reset
 commit 3 - html
 commit 4 - css
+commit 5 - constructor
--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 'use strict'
 
 console.log('Hello World');
+
+//stuff and things
+// constructor for tha imgs
+
+function Image(name, src) {
+  this.viewed = 0;
+  this.clicked = 0;
+  this.src = src;
+  this.name = name;
+}
