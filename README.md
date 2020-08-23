d7449f7174fc505c9277dcdacb879ce39f5ad076
kgonzalez1219 <kat@MSI.localdomain>
2020-08-22 18:18:27 -0700

    add objects

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ commit 2 - images and reset
 commit 3 - html
 commit 4 - css
 commit 5 - constructor
+commit 6 - array
+commit 7 - objects
--- app.js
+++ app.js
@@ -16,3 +16,27 @@ function Image(name, src) {
 
   imgArray.push(this);
 }
+
+//tha objects
+
+new Image ('bag', './img/bag.jpg');
+new Image ('banana', './img/banana.jpg');
+new Image ('bathroom', './img/bathroom.jpg');
+new Image ('boots', './img/boots.jpg');
+new Image ('breakfast', './img/breakfast.jpg');
+new Image ('bubblegum', './img/bubblegum.jpg');
+new Image ('chair', './img/chair.jpg');
+new Image ('cthulhu', './img/cthulhu.jpg');
+new Image ('dog-duck', './img/dog-duck.jpg');
+new Image ('dragon', './img/dragon.jpg');
+new Image ('pen', './img/pen.jpg');
+new Image ('pet-sweep', './img/pet-sweep.jpg');
+new Image ('scissors', './img/scissors.jpg');
+new Image ('shark', './img/shark.jpg');
+new Image ('sweep', './img/sweep.png');
+new Image ('tauntaun', './img/tauntaun.jpg');
+new Image ('unicorn', './img/unicorn.jpg');
+new Image ('usb', './img/usb.gif');
+new Image ('water-can', './img/water-can.jpg');
+new Image ('wine-glass', './img/wine-glass.jpg');
+console.log(imgArray);
