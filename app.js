b02e831d96f99200487e89d6428d9d0a98ab2f7e
kgonzalez1219 <kat@MSI.localdomain>
2020-08-25 14:24:45 -0700

    nohing working

--- app.js
+++ app.js
@@ -5,9 +5,9 @@
 //global shtuff
 var imgArray = [];
 
-var imageElOne  = document.getElementById('image-one');
+var imageElOne = document.getElementById('image-one');
 var imageElTwo = document.getElementById('image-two');
-var imageElThree = document.getElementById('image-three');
+//var imageElThree = document.getElementById('image-three');
 
 //stuff and things
 // constructor for tha imgs
@@ -23,53 +23,46 @@ function Picture(name, src) {
 
 //tha objects
 
-new Picture('bag', './img/bag.jpg');
-new Picture('banana', './img/banana.jpg');
-new Picture('bathroom', './img/bathroom.jpg');
-new Picture('boots', './img/boots.jpg');
-new Picture('breakfast', './img/breakfast.jpg');
-new Picture('bubblegum', './img/bubblegum.jpg');
-new Picture('chair', './img/chair.jpg');
-new Picture('cthulhu', './img/cthulhu.jpg');
-new Picture('dog-duck', './img/dog-duck.jpg');
-new Picture('dragon', './img/dragon.jpg');
-new Picture('pen', './img/pen.jpg');
-new Picture('pet-sweep', './img/pet-sweep.jpg');
-new Picture('scissors', './img/scissors.jpg');
-new Picture('shark', './img/shark.jpg');
-new Picture('sweep', './img/sweep.png');
-new Picture('tauntaun', './img/tauntaun.jpg');
-new Picture('unicorn', './img/unicorn.jpg');
-new Picture('usb', './img/usb.gif');
-new Picture('water-can', './img/water-can.jpg');
-new Picture('wine-glass', './img/wine-glass.jpg');
+new Picture('bag','./img/bag.jpg');
+new Picture('banana','./img/banana.jpg');
+new Picture('bathroom','./img/bathroom.jpg');
+new Picture('boots','./img/boots.jpg');
+//new Picture('breakfast', './img/breakfast.jpg');new Picture('bubblegum', './img/bubblegum.jpg');new Picture('chair', './img/chair.jpg');
+//new Picture('cthulhu', './img/cthulhu.jpg');new Picture('dog-duck', './img/dog-duck.jpg');new Picture('dragon', './img/dragon.jpg');new Picture('pen', './img/pen.jpg');new Picture('pet-sweep', './img/pet-sweep.jpg');new Picture('scissors', './img/scissors.jpg');new Picture('shark', './img/shark.jpg');new Picture('sweep', './img/sweep.png');new Picture('tauntaun', './img/tauntaun.jpg');new Picture('unicorn', './img/unicorn.jpg');new Picture('usb', './img/usb.gif');new Picture('water-can', './img/water-can.jpg');new Picture('wine-glass', './img/wine-glass.jpg');
 
 //console.log(imgArray);
 
 //function time
 function renderImages() {
-  var imgOne = imgArray [randomNumber(imgArray.length)];
+   var imgOne = imgArray[randomNumber(imgArray.length)];
+   var imgTwo = imgArray[randomNumber(imgArray.length)];
+  //var imgThree = imgArray[randomNumber(imgArray.length)];
 
-  var imgTwo = imgArray [randomNumber(imgArray.length)];
+  
 
-  var imgThree = imgArray [randomNumber (imgArray.length)];
-
-  while(imgOne === imgTwo || imageElThree) {
-    imgTwo.src = imgArray[randomNumber(imgArray.length)];
+  while(imgOne === imgTwo) {
+    var imgTwo = imgArray[randomNumber(imgArray.length)];
   }
 
   imageElOne.src = imgOne.src;
   imageElTwo.src = imgTwo.src;
-  imageElThree.src = imgThree.src;
+  //imageElThree.src = imgThree.src;
 
   imageElOne.alt = imgOne.name;
   imageElTwo.alt = imgTwo.name;
-  imageElThree.alt = imgThree.name;
+  //imageElThree.alt = imgThree.name;
+
+  imgOne.viewed++;
+  imgTwo.viewed++;
+  //imgThree.viewd++;
 
 }
 
 //random numbers
-function randomNumber() {
-  return Math.floor(Math.random() * max)
+function randomNumber(max) {
+  return Math.floor(Math.random() * max);
 }
+
+renderImages();
+
 
