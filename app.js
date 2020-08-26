5133c55c918f94903bc0ac3d842b718677e8905a
kgonzalez1219 <kat@MSI.localdomain>
2020-08-25 18:16:46 -0700

    event listener

--- app.js
+++ app.js
@@ -17,16 +17,15 @@ function Picture(name, src) {
   this.clicked = 0;
   this.src = src;
   this.name = name;
-
   imgArray.push(this);
 }
 
 //tha objects
 
-new Picture('bag','./img/bag.jpg');
-new Picture('banana','./img/banana.jpg');
-new Picture('bathroom','./img/bathroom.jpg');
-new Picture('boots','./img/boots.jpg');
+new Picture('bag', './img/bag.jpg' );
+new Picture('banana', './img/banana.jpg' );
+new Picture('bathroom', './img/bathroom.jpg' );
+new Picture('boots', './img/boots.jpg' );
 //new Picture('breakfast', './img/breakfast.jpg');new Picture('bubblegum', './img/bubblegum.jpg');new Picture('chair', './img/chair.jpg');
 //new Picture('cthulhu', './img/cthulhu.jpg');new Picture('dog-duck', './img/dog-duck.jpg');new Picture('dragon', './img/dragon.jpg');new Picture('pen', './img/pen.jpg');new Picture('pet-sweep', './img/pet-sweep.jpg');new Picture('scissors', './img/scissors.jpg');new Picture('shark', './img/shark.jpg');new Picture('sweep', './img/sweep.png');new Picture('tauntaun', './img/tauntaun.jpg');new Picture('unicorn', './img/unicorn.jpg');new Picture('usb', './img/usb.gif');new Picture('water-can', './img/water-can.jpg');new Picture('wine-glass', './img/wine-glass.jpg');
 
@@ -41,7 +40,7 @@ function renderImages() {
   
 
   while(imgOne === imgTwo) {
-    var imgTwo = imgArray[randomNumber(imgArray.length)];
+     imgTwo = imgArray[randomNumber(imgArray.length)];
   }
 
   imageElOne.src = imgOne.src;
@@ -61,6 +60,19 @@ function renderImages() {
 //random numbers
 function randomNumber(max) {
   return Math.floor(Math.random() * max);
+}
+
+//event handler for tha broken thing
+imageElOne.addEventListener('click', eventHandler);
+imageElTwo.addEventListener('click', eventHandler);
+
+function eventHandler(event) {
+  console.log('immma throw my comp');
+  for(var i = 0; i < imgArray.length; i++) {
+    if (imgArray[i].name === event.target.alt); {
+      imgArray[i].clicked++;
+    }
+  }
 }
 
 renderImages();
